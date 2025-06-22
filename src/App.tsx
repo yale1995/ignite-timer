7e9ed2fd3c6f9cbd6ed16db1dc887b02468c1c39
Yale Araujo <yale850@gmail.com>
2025-06-22 19:13:33 -0300

    chore: setting font-families, global styles and the default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>App</h1>
+      <GlobalStyle/>
     </ThemeProvider>
   );
 };
