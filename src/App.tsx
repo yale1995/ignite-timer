2d9113f5f0d2f473da49cc16eb9357694bbeef53
Yale Araujo <yale850@gmail.com>
2025-06-22 18:47:35 -0300

    chore: setting theme using styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
 export const App = () => {
-  return <h1>App</h1>
-}
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>App</h1>
+    </ThemeProvider>
+  );
+};
