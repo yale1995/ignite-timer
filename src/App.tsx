66eda1347092bb435ccd4a3b89d97d058590a2fa
Yale Araujo <yale850@gmail.com>
2025-06-22 19:26:25 -0300

    chore: setting biome code formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export const App = () => {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <h1>App</h1>
-      <GlobalStyle/>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<h1>App</h1>
+			<GlobalStyle />
+		</ThemeProvider>
+	);
 };
