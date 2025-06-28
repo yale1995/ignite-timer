a7599c56275bf5c1b4f95127808caf0218e7d68a
Yale Araujo <yale850@gmail.com>
2025-06-27 21:28:13 -0300

    feat: enhance Home component layout with styled containers for form and countdown, and add start button functionality

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,41 @@
+import { Play } from "phosphor-react";
+import {
+	CountdownContainer,
+	FormContainer,
+	HomeContainer,
+	Separator,
+} from "./styles";
+
 export function Home() {
 	return (
-		<div>
+		<HomeContainer>
 			<form>
-				<label htmlFor="task">Vou trabalhar em</label>
-				<input
-					type="text"
-					id="task"
-					placeholder="Dê um nome para o seu projeto"
-				/>
-				<label htmlFor="minutesAmount">durante</label>
-				<input type="number" id="minutesAmount" placeholder="00" />
-				<span>minutos.</span>
+				<FormContainer>
+					<label htmlFor="task">Vou trabalhar em</label>
+					<input
+						type="text"
+						id="task"
+						placeholder="Dê um nome para o seu projeto"
+					/>
+
+					<label htmlFor="minutesAmount">durante</label>
+					<input type="number" id="minutesAmount" placeholder="00" />
+					<span>minutos.</span>
+				</FormContainer>
+
+				<CountdownContainer>
+					<span>0</span>
+					<span>0</span>
+					<Separator>:</Separator>
+					<span>0</span>
+					<span>0</span>
+				</CountdownContainer>
+
+				<button type="submit">
+					<Play size={24} />
+					Começar
+				</button>
 			</form>
-		</div>
+		</HomeContainer>
 	);
 }
